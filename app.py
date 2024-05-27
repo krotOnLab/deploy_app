4ac488deecc246d7b1369b37490b5afd96bea1bb
jDS_krotOn <thisprostopochtamy@gmail.com>
2024-05-27 14:13:30 +0300

    t

--- app.py
+++ app.py
@@ -12,59 +12,12 @@ locale.setlocale(locale.LC_ALL, 'ru_RU')
 app = Flask(__name__) 
 
 
-def calc_period (dates):
-    dates = dates.split(',')
-    dates = [i.split('.') for i in dates]
-    start_date = datetime(int(dates[0][2]), int(dates[0][1]), int(dates[0][0]))
-    end_date = datetime(int(dates[1][2]), int(dates[1][1]), int(dates[1][0]))
-    current_date = start_date
-    date_list = []
-    while current_date <= end_date:
-        formatted_date = current_date.strftime('%d - %B - %A')
-        date_list.append({"day": formatted_date.split(' - ')[0],
-                          "month": formatted_date.split(' - ')[1], "day_week":formatted_date.split(' - ')[2]})
-        current_date += timedelta(days=1)
-    return date_list
 
 
 @app.route('/')
 def hello_world():  # put application's code here
     return render_template('authorization.html')
 
-
-def enter_lk(login):
-    trips = select_trips(login)
-    return render_template('lk.html', trips=trips, number_trips=len(trips))  # render lk
-
-
-@app.route('/submit_auth', methods=['post', 'get'])
-def main_page():
-    global login_user
-    login = request.form.get('username').replace(" ", "").replace("\t", "")
-    password = request.form.get('password').replace(" ", "").replace("\t", "")
-    if login == 'master' and password == 'master_pas':
-        login_user = login
-        return enter_lk(login)
-    if authorization(app, login, password):
-        login_user = login
-        return enter_lk(login)
-    else:
-        return render_template('authorization.html', answer=False)
-
-
-
-
-
-
-
-
-
-
-
-if __name__ == '__main__':
-    app.run(host="tg-travel-helper-bot.local", ssl_context=("localhost.pem", "localhost-key.pem"))
-
-    
     
     
     
